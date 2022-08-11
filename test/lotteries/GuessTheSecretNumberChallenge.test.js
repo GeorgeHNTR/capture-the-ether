25f786362f14d9589e1f8eaf9d71537242b71fc7
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-08-11 03:35:51 +0300

    solve GuessTheSecretNumberChallenge

--- test/lotteries/GuessTheSecretNumberChallenge.test.js
+++ test/lotteries/GuessTheSecretNumberChallenge.test.js
@@ -1,7 +1,8 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const keccak256 = require('keccak256');
 
-describe("GuessTheSecretNumberChallenge", async function () {
+describe.only("GuessTheSecretNumberChallenge", async function () {
     let contract;
 
     before(async function () {
@@ -10,6 +11,16 @@ describe("GuessTheSecretNumberChallenge", async function () {
 
     it("Exploit", async function () {
         // WRITE YOUR CODE HERE
+
+        const answerHash = await ethers.provider.getStorageAt(contract.address, 0);
+
+        // Since the input parameter is of type uint8, all possible values are in the range 0 to 255, so we can easily brute force it
+        for (let n = 0; n < 256; n++) {
+            if ("0x".concat(keccak256(n).toString("hex")) === answerHash) {
+                await contract.guess(n, { value: ethers.utils.parseEther("1") });
+                break;
+            }
+        }
     });
 
     after(async function () {
